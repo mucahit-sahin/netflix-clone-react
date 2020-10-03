16a711e81f689f3df5af34484dc3b9f8e9b4655a
mucahit-sahin <mucahitsahin786@gmail.com>
2020-10-03 22:31:11 +0300

    trailer modalı sorunu çözüldü

--- src/components/row/Row.js
+++ src/components/row/Row.js
@@ -3,18 +3,15 @@ import React from "react";
 import axios from "../../axios";
 import "./Row.css";
 
-import Modal from "react-modal";
-import YouTube from "react-youtube";
 import movieTrailer from "movie-trailer";
+import ModalVideo from "react-modal-video";
 
 const img_base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchUrl, isLargeRow }) {
-  Modal.setAppElement("div");
   const [movies, setMovies] = useState([]);
-  const [modalIsOpen, setIsOpen] = useState(false);
-  const [modalMovie, setModalMovie] = useState({});
   const [trailerUrl, setTrailerUrl] = useState("");
+  const [isOpen, setOpen] = useState(false);
 
   useEffect(() => {
     async function fetchData() {
@@ -26,27 +23,12 @@ function Row({ title, fetchUrl, isLargeRow }) {
   }, [fetchUrl]);
 
   function openModal(movie) {
-    setModalMovie(movie);
     movieTrailer(movie.original_title || "").then((url) => {
       const urlParams = new URLSearchParams(new URL(url).search);
       setTrailerUrl(urlParams.get("v"));
     });
-
-    setIsOpen(true);
+    setOpen(true);
   }
-  function closeModal() {
-    setIsOpen(false);
-  }
-
-  const opts = {
-    maxheight: "100",
-    width: "100%",
-    position: "absolute",
-    playerVars: {
-      // https://developers.google.com/youtube/player_parameters
-      autoplay: 1,
-    },
-  };
 
   return (
     <div className="row">
@@ -63,17 +45,14 @@ function Row({ title, fetchUrl, isLargeRow }) {
             alt={movie.original_title}
           ></img>
         ))}
+        <ModalVideo
+          channel="youtube"
+          autoplay
+          isOpen={isOpen}
+          videoId={trailerUrl}
+          onClose={() => setOpen(false)}
+        />
       </div>
-      <Modal
-        isOpen={modalIsOpen}
-        onRequestClose={closeModal}
-        className="modal"
-        contentLabel="Example Modal"
-      >
-        <button onClick={closeModal}>Close</button>
-        <h2>{modalMovie.original_title}</h2>
-        <YouTube videoId={trailerUrl} opts={opts} />
-      </Modal>
     </div>
   );
 }
